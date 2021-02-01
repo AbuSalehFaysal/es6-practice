ad47a3ade35642a51adbc306f6be2686a81da87e
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2021-02-02 00:23:08 +0600

    template.js added

---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "Abu Saleh";
+const lastName = "Faysal";
+const fullName = firstName + lastName;
+console.log(fullName);
+const fullName2 = `${firstName} ${lastName}`;
+console.log(fullName2);
+
+const multiline = "Abu"
+    + "Saleh"
+    + "Faysal";
+console.log(multiline);
+const multiline2 = `Abu
+Saleh
+Faysal`;
+console.log(multiline2);
