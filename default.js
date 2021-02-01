d6509fa8895c04767a9cafbe36a462939bcde8d8
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2021-02-02 00:03:20 +0600

    letconst.js and default.js Added

---
+++ default.js
@@ -0,0 +1,6 @@
+function sum(num1, num2 = 0){ // num2 = 0 if user does not give the value of num2
+    return num1+num2;
+}
+
+const total = sum(15);
+console.log(total);
---
+++ letconst.js
@@ -0,0 +1,8 @@
+const userName = "Abu Saleh Faysal";
+console.log(userName);
+
+const array = [10,20,30,40,50]
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element);
+}
